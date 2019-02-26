271167706f8aca332569c6691e58f299bd93034f
xiongjian <webjsforyou@163.com>
2019-02-26 11:53:36 +0800

    搭建了antd和react-router的环境

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Login from './pages/login';
+import Admin from './pages/admin';
+
+export default class App extends Component {
+  
+  render () {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <Route path='/' component={Admin}/>
+        </Switch>
+      </Router>
+    )
+  }
+}
