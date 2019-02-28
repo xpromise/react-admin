08ec9d87f2b1d82bbe4cc8ee5bb15bc95d31ad58
xiongjian <webjsforyou@163.com>
2019-02-28 14:55:41 +0800

    完成header的显示用户信息、登出、显示标签title功能

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,78 @@
 import React, {Component} from 'react';
+import {Row, Col, Modal} from 'antd';
+import {withRouter} from 'react-router-dom';
 
+import MemoryUtils from '../../utils/memoryUtils';
+import {removeItem} from '../../utils/storageUtils';
+import menuList from '../../config/menuConfig';
 import './index.less';
 
-export default class Header extends Component {
+class Header extends Component {
+  
+  //退出登录方法
+  logOut = () => {
+    Modal.confirm({
+      title: '您确认要退出登录吗?',
+      okText: '确认',
+      cancelText: '取消',
+      onOk: () => {
+        //点击确认时触发回调函数
+        //清除用户信息（localStorage 内存）
+        removeItem();
+        MemoryUtils.user = {};
+        //返回到登录页面
+        this.props.history.replace('/login');
+      },
+    });
+  }
+  
+  //获取标题方法
+  getTitle = menu => {
+    const {pathname} = this.props.location;
+    
+    for (let i = 0; i < menu.length; i++) {
+      let item = menu[i];
+      if (item.children) {
+        /*for (let j = 0; j < item.children.length; j++) {
+          let cItem = item.children[j];
+          if (cItem.key === pathname) {
+            return cItem.title;
+          }
+          
+        }*/
+        //递归去找是否有匹配的title，如果有才返回
+        const title = this.getTitle(item.children);
+        if (title) {
+          return title;
+        }
+      } else {
+        if (item.key === pathname) {
+          return item.title;
+        }
+      }
+    }
+  }
+  
   render () {
+    //获取当前用户信息
+    const {username} = MemoryUtils.user
+    //获取标题
+    const title = this.getTitle(menuList);
+    
     return (
       <div className='header'>
-        header
+        <Row className='header-top'>
+          <span>欢迎, {username}</span>
+          <a href="javascript:void(0);" onClick={this.logOut}>退出</a>
+        </Row>
+        <Row className='header-bottom'>
+          <Col span={6} className='header-bottom-left'>{title}</Col>
+          <Col span={18} className='header-bottom-right'>时间 + 天气</Col>
+        </Row>
       </div>
     )
   }
 }
+
+
+export default withRouter(Header);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import App from './App';
-import {getItem} from './utils/storageUtils';
-import MemoryUtils from './utils/memoryUtils';
+import {getItem} from "./utils/storageUtils";
+import MemoryUtils from "./utils/memoryUtils";
 
 //将localStorage的值读取出来，保存在内存中
 const user = getItem();
